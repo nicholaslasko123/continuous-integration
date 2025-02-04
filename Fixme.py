f02c8a63be9f8520bf261e99915893e0bb2b0ebb
nicholaslasko123 <130191883+nicholaslasko123@users.noreply.github.com>
2025-02-04 09:44:36 -0800

    Update Fixme.py

--- Fixme.py
+++ Fixme.py
@@ -44,4 +44,7 @@ def triangular(n):
     >>> triangular(400)
     80200
     '''
-    return n * (n + 1) // 2  # Using the formula for the nth triangular number
+    result = 1
+    for i in range(2, n + 1):
+        result += i
+    return result
